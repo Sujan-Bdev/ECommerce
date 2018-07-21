3c20da07c555e3ab9913e1ed489876e1c0c610bc
Nbn PK <nabin62246@gmail.com>
2018-07-21 15:31:45 +0545

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -6,3 +6,8 @@ urlpatterns = [
     path('', views.home, name='index'),
     path('sample/', views.sample, name='sample')
 ]
+
+urlpatterns+=[
+    path('signup/customer',views.CustomerSignUpView.as_view(),name='signup_customer'),
+    path('signup/staff',views.StaffSignUpView.as_view(),name='signup_admin'),
+]
